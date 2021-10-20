08db305f7384a089965f1d17a6c4be0a8366d07f
zhangshixiang89 <76580524+zhangshixiang89@users.noreply.github.com>
2021-10-20 11:35:16 +0800

    Update jd_redPacket.js

--- jd_redPacket.js
+++ jd_redPacket.js
@@ -24,7 +24,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
-$.redPacketId = [];
+$.redPacketId = ['E2jJrBI8OFgRFZ'];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
