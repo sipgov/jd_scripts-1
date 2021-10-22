a446dd4648277bbf2c2d79884dead07125a22ece
zhangshixiang89 <76580524+zhangshixiang89@users.noreply.github.com>
2021-10-22 12:30:20 +0800

    Update jd_hyj.js

--- jd_hyj.js
+++ jd_hyj.js
@@ -9,7 +9,7 @@ let cookiesArr = [],
     cookie = '',
     message;
 let secretp = '',
-    inviteId = []
+    inviteId = ['ZXASTT0225KkcRhlK9VOBJB_zwPBefAFjRWn6u7zB55awQ']
 
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
